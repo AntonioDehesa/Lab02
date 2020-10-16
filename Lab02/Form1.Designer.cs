33e6de34146b8de29ff7db7fad236dd9bf276489
Heriberto Antonio Dehesa Ortiz <hdehesaortiz@gmail.com>
2020-10-15 21:54:39 -0500

    Agregar archivos de proyecto.

---
+++ Lab02/Form1.Designer.cs
@@ -0,0 +1,120 @@
+﻿namespace Lab02
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.Send = new System.Windows.Forms.Button();
+            this.Clear = new System.Windows.Forms.Button();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.tbTX = new System.Windows.Forms.RichTextBox();
+            this.tbRX = new System.Windows.Forms.RichTextBox();
+            this.mySerialPort = new System.IO.Ports.SerialPort(this.components);
+            this.SuspendLayout();
+            // 
+            // Send
+            // 
+            this.Send.Location = new System.Drawing.Point(665, 12);
+            this.Send.Name = "Send";
+            this.Send.Size = new System.Drawing.Size(98, 23);
+            this.Send.TabIndex = 0;
+            this.Send.Text = "Send";
+            this.Send.UseVisualStyleBackColor = true;
+            this.Send.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Clear
+            // 
+            this.Clear.Location = new System.Drawing.Point(665, 397);
+            this.Clear.Name = "Clear";
+            this.Clear.Size = new System.Drawing.Size(98, 23);
+            this.Clear.TabIndex = 1;
+            this.Clear.Text = "Clear";
+            this.Clear.UseVisualStyleBackColor = true;
+            this.Clear.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(665, 69);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(44, 21);
+            this.checkBox1.TabIndex = 2;
+            this.checkBox1.Text = "IM";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // tbTX
+            // 
+            this.tbTX.Location = new System.Drawing.Point(12, 12);
+            this.tbTX.Name = "tbTX";
+            this.tbTX.Size = new System.Drawing.Size(647, 96);
+            this.tbTX.TabIndex = 3;
+            this.tbTX.Text = "";
+            this.tbTX.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.tbTX_KeyPress);
+            // 
+            // tbRX
+            // 
+            this.tbRX.Location = new System.Drawing.Point(12, 114);
+            this.tbRX.Name = "tbRX";
+            this.tbRX.Size = new System.Drawing.Size(647, 306);
+            this.tbRX.TabIndex = 4;
+            this.tbRX.Text = "";
+            this.tbRX.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.tbRX_KeyPress);
+            // 
+            // mySerialPort
+            // 
+            this.mySerialPort.DataReceived += new System.IO.Ports.SerialDataReceivedEventHandler(this.mySerialPort_DataReceived);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.tbRX);
+            this.Controls.Add(this.tbTX);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.Clear);
+            this.Controls.Add(this.Send);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Send;
+        private System.Windows.Forms.Button Clear;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.RichTextBox tbTX;
+        private System.Windows.Forms.RichTextBox tbRX;
+        private System.IO.Ports.SerialPort mySerialPort;
+    }
+}
+
---
+++ Lab02/Form1.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab02
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            if(!mySerialPort.IsOpen)
+            {
+                mySerialPort.Open();
+                tbRX.Text = "port opened";
+            }
+            else
+            {
+                tbRX.Text = "port busy :(";
+            }
+        }
+        private string rxString;
+        private void mySerialPort_DataReceived(object sender, System.IO.Ports.SerialDataReceivedEventArgs e)
+        {
+            rxString = mySerialPort.ReadExisting();
+            this.Invoke(new EventHandler(displayText));
+        }
+
+        private void displayText(object o, EventArgs e)
+        {
+            tbRX.AppendText(rxString);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            mySerialPort.Write(tbTX.Text);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            tbTX.Clear();
+            tbRX.Clear();
+        }
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            mySerialPort.Close();
+        }
+
+        private void tbTX_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (mySerialPort.IsOpen && checkBox1.Checked)
+            {
+                char[] ch = new char[1];
+                ch[0] = e.KeyChar;
+                mySerialPort.Write(ch, 0, 1);
+            }
+        }
+
+        private void tbRX_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            
+        }
+    }
+}
